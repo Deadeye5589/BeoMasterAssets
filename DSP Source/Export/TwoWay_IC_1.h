1986354929d94a6f31ed84ab4b336f54b05b5a2a
Deadeye5589 <Deadeye5589@hotmail.com>
2026-01-30 09:47:55 +0100

    DSP Configuration
    Complete DSP configuration for the BeoMaster Amp
    Works with SigmaStudio 4.7

---
+++ DSP Source/Export/TwoWay_IC_1.h
@@ -0,0 +1,38 @@
+/*
+ * File:           C:\Users\Succubus\Documents\KiCad\B&O Master\DSP Source\Export\TwoWay_IC_1.h
+ *
+ * Created:        Thursday, December 28, 2023 4:48:27 PM
+ * Description:    TwoWay:IC 1 program data.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This software may only be used to program products purchased from
+ * Analog Devices for incorporation by you into audio products that
+ * are intended for resale to audio product end users. This software
+ * may not be distributed whole or in any part to third parties.
+ *
+ * Copyright ©2023 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __TWOWAY_IC_1_H__
+#define __TWOWAY_IC_1_H__
+
+#include "SigmaStudioFW.h"
+#include "TwoWay_IC_1_REG.h"
+
+#define DEVICE_ARCHITECTURE_IC_1                  "E2Prom"
+#define DEVICE_ADDR_IC_1                          0xA0
+
+
+
+/*
+ * Default Download
+ */
+#define DEFAULT_DOWNLOAD_SIZE_IC_1 0
+
+void default_download_IC_1() {
+}
+
+#endif
